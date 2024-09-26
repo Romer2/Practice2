fa7e75b157e173bf65e8c9e9fe88e0750e8479fe
“Romer2” <Destract1923@gmail.com>
2024-09-26 16:20:22 +0300

    Pr2

---
+++ Assets/MainJS/javascript.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// 1 task
+
+ let name = 'Джон';
+ let admin = name;
+ alert(admin);
+
+//  2 task
+
+ let city = prompt('Название города');
+ let year = prompt('Год основания');
+ let peoples = prompt('Населенеие');
+ 
+ alert(`Городу исполнилось ${city} исполнилось ${year} лет с момента его образования. ${peoples} человек.`);
+
+//  3 task
+
+    let rad = prompt("Напишите радиус");
+    let squre = 3.14 * rad * rad;
+    alert(squre);
+
+// 4 task 
+ 
+    let a = prompt('Напишите первое число');
+    let b = prompt('Напишите второе число');
+
+    let sum = a + b;
+    let raz = a - b;
+    let chas = a / b;
+    let proiz = a * b;
+
+    alert(`Сумма = ${sum}, Разность = ${raz}, Частное = ${chas}, Произведение = ${proiz}`);
+
